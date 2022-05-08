ab73b384357d23e3011d0d3c90e38f4bef0e24a1
Denes Beck <denes.beck@hu.ibm.com>
2022-05-08 15:52:46 +0200

    footer buttons small fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,15 +5,21 @@ const Footer: React.FC = () => {
         <footer className='relative bottom-0 mt-auto flex w-full items-center justify-start py-10 px-2 text-slate-200 sm:px-8'>
             <div className='mr-auto text-sm'>2022 | Created by Denes Beck</div>
             <div className='flex space-x-6'>
-                <a href='https://github.com/denesbeck' target='_blank' rel='noreferrer'>
-                    <button className='h-7 w-7 rounded-full outline-none outline-offset-4 hover:text-slate-400 focus-visible:outline-blue-300'>
-                        <BsGithub className='h-full w-full' />
-                    </button>
+                <a
+                    href='https://github.com/denesbeck'
+                    target='_blank'
+                    rel='noreferrer'
+                    className='h-7 w-7 rounded-full outline-none outline-offset-4 hover:text-slate-400 focus-visible:outline-blue-300'
+                >
+                    <BsGithub className='h-full w-full' />
                 </a>
-                <a href='https://twitter.com/denesbeck' target='_blank' rel='noreferrer'>
-                    <button className='h-7 w-7 rounded-full outline-none outline-offset-4 hover:text-slate-400 focus-visible:outline-blue-300'>
-                        <BsTwitter className='h-full w-full' />
-                    </button>
+                <a
+                    href='https://twitter.com/denesbeck'
+                    target='_blank'
+                    rel='noreferrer'
+                    className='h-7 w-7 rounded-full outline-none outline-offset-4 hover:text-slate-400 focus-visible:outline-blue-300'
+                >
+                    <BsTwitter className='h-full w-full' />
                 </a>
             </div>
         </footer>
